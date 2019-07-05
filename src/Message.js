fac1b360b5942f4a4f35c9aea62551854bf1c74a
Jason Fournier <jmf4nier@hotmail.com>
2019-07-05 14:28:15 -0500

    finished

--- src/Message.js
+++ src/Message.js
@@ -1,1 +1,15 @@
-
+class Message{
+    static counter = 0;
+    constructor(content){
+        this.content = content
+        this.render()
+    }
+    render(){
+        let list = document.querySelector('#messages')
+        let newLi = document.createElement('li')
+        newLi.innerText = this.content
+        list.append(newLi)
+        Message.counter++
+        // console.log(Message.counter);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
+function getMessages(url){
+   fetch(url)  
+   .then(rsp=>rsp.json())
+   .then(result=> {
+      for(let i = Message.counter; i<result.length; i++){
+         /* class counter for messages.  only instantiates( and renders) if there 
+         are more messages in the fetch response than the counter.*/
+         new Message(result[i].content)
+      }
+   })
+}
+
+document.addEventListener('DOMContentLoaded', (e)=>{
+
+   setInterval(()=>{
+   let list = document.querySelector('#messages')
+   getMessages('http://10.185.1.218:3000/messages')
+}, 500)
+let submitButton = document.querySelector('#submit-btn')
+
+getMessages('http://10.185.1.218:3000/messages')
+
+submitButton.addEventListener('click', e =>{
+   e.preventDefault()
+   let inputField = document.querySelector('#message_input')
+   if (inputField.value != ""){
+      new Message(inputField.value)
+      
+      fetch('http://10.185.1.218:3000/messages', {
+         method: 'POST', 
+         headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+         },
+      body: JSON.stringify( {
+         content: inputField.value
+      })
+      })
+      inputField.value = ''
+   }
+   
+})
+
+
+
+
+
+})
 
